04275b93a69db911d538a4cf338f6c1e52bbaf71
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-25 15:13:04 -0600

    adding tests for expressions and multiline strings

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -16,5 +16,26 @@ describe('template strings', function(){
 
       expect(string).to.be.a('string');
   });
+  
+  it('can evaluate variables', function(){
+    var bar = 'baz'
+    var string = `foo ${bar}`;
+
+
+    expect(string).to.equal('foo baz');
+  })
+   
+  it('can execute functions', function(){
+    var string = `the date is: ${new Date().getFullYear()}`;
+
+    var date = new Date().getFullYear();
+
+    expect(string).to.equal("the date is: "+date);
+  });
+
+  it('can execute expressions', function(){
+  
+  });
+
 });
 
